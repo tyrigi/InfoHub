2a7a1ca96a4534e1a05ca1850de4b56222b2e0f7
Tyler Gilbert <gilbertt@oregonstate.edu>
2017-03-16 13:20:48 -0700

    Added label key and a quick script for trying things out in the interpeter.
    Signed-off-by: Tyler Gilbert <gilbertt@oregonstate.edu>

---
+++ quickauth.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/gmail-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Quickstart'
+
+
+home_dir = os.path.expanduser('~')
+credential_dir = os.path.join(home_dir, '.credentials')
+if not os.path.exists(credential_dir):
+    os.makedirs(credential_dir)
+credential_path = os.path.join(credential_dir,'gmail-python-quickstart.json')
+
+store = Storage(credential_path)
+credentials = store.get()
+if not credentials or credentials.invalid:
+    flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+    flow.user_agent = APPLICATION_NAME
+    if flags:
+        credentials = tools.run_flow(flow, store, flags)
+    else: # Needed only for compatibility with Python 2.6
+        credentials = tools.run(flow, store)
+    print('Storing credentials to ' + credential_path)
+
+
