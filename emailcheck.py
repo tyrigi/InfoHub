5d6e6ef2dfde6894a6d73b5d76f9e6c02bd78e97
Tyler Gilbert <tyrigi@gmail.com>
2017-03-16 23:46:36 -0700

    Fixed indexing problem with removing closed bugs.
    Signed-off-by: Tyler Gilbert <tyrigi@gmail.com>

--- emailcheck.py
+++ emailcheck.py
@@ -37,6 +37,43 @@ def get_credentials():
         print('Storing credentials to '+ credential_path)
     return credentials
 
+def add_and_update(bug_list, person, bug_number, date):
+    new_bug = 1 # 0 - not a new bug 1 - new bug
+    for bug_entry in bug_list:
+        if bug_entry['internalDate'] == date:
+            #message has been dealt with
+            print(bug_number," repeated message")
+            new_bug = 0
+            break
+        else:
+            # new message
+            if bug_entry['number'] == bug_number:
+                # bug is already in the list
+                new_bug = 0
+                if int(bug_entry['internalDate']) < int(date):
+                    # current message happened after bug's last update
+                    if bug_entry['assignment'] == person:
+                        # bug is already assigned to this person
+                        print("already assigned to person")
+                        break
+                    else:
+                        # bug assignment changed
+                        print(bug_number, " changed assignment")
+                        bug_entry['assignment'] = person
+                        bug_entry['internalDate'] = date
+                        break
+                else:
+                    #old news, doesn't matter
+                    print(bug_number, " is already in list and up-to-date")
+                    break
+            else: 
+                # no match
+                continue
+    if new_bug:
+        print(bug_number," is new")
+        bug_list.append({'internalDate': date, 'number': bug_number, 'assignment': person})   
+    return bug_list
+
 def get_bug_assignments(service):
     bug_list = []
     bug_messages = service.users().messages().list(userId='me', labelIds='Label_7').execute().get('messages', [])
@@ -50,62 +87,50 @@ def get_bug_assignments(service):
             person = 'Unassigned'
         bug_number = re.search('\w+-\w+', message_subject[message_subject.find('Assigned: ')+10:]).group(0)
         date = bug_message['internalDate']
-        new_bug = 1 # 0 - not a new bug 1 - new bug
-        for bug_entry in bug_list:
-            if bug_entry['internalDate'] == date:
-                #message has been dealt with
-                print(bug_number," repeated message")
-                new_bug = 0
-                break
-            else:
-                # new message
-                if bug_entry['number'] == bug_number:
-                    # bug is already in the list
-                    new_bug = 0
-                    if int(bug_entry['internalDate']) < int(date):
-                        # current message happened after bug's last update
-                        if bug_entry['assignment'] == person:
-                            # bug is already assigned to this person
-                            print("already assigned to person")
-                            break
-                        else:
-                            # bug assignment changed
-                            print(bug_number, " changed assignment")
-                            bug_entry['assignment'] = person
-                            bug_entry['internalDate'] = date
-                            break
-                    else:
-                        #old news, doesn't matter
-                        print(bug_number, " is already in list and up-to-date")
-                        break
-                else: 
-                    # no match
-                    continue
-        if new_bug:
-            print(bug_number," is new")
-            bug_list.append({'internalDate': date, 'number': bug_number, 'assignment': person})
+        bug_list = add_and_update(bug_list, person, bug_number, date)
     return bug_list
 
-def get_closed_bugs(service, assigned_bugs):
+def get_closed_bugs(service):
+    bug_list = []
     bug_messages = service.users().messages().list(userId='me', labelIds='Label_11').execute().get('messages', [])
     for bugs in bug_messages:
-        bug_subject = service.users().messages().get(userId='me', id=bugs['id'], format='metadata', metadataHeaders='Subject').execute().get('payload', []).get('headers', [])[0]['value']
-        bug_number = re.search('\w+-\w+', bug_subject[bug_subject.find('Assigned: ')+10:]).group(0)
-        try:
-            assigned_bugs.remove(bug_number)
-            print(bug_number)
-        except Exception as error:
-            print(error)
-    return assigned_bugs
+        bug_message = service.users().messages().get(userId='me', id=bugs['id']).execute()
+        message_subject = bug_message.get('payload', []).get('headers', [])[24]['value']
+        bug_number = re.search('\w+-\w+', message_subject[message_subject.find('Assigned: ')+10:]).group(0)
+        date = bug_message['internalDate']
+        bug_list = add_and_update(bug_list, None, bug_number, date)
+    return bug_list
 
 def main():
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
     assigned_bugs = get_bug_assignments(service)
+    closed_bugs = get_closed_bugs(service)
+    bugs_to_remove = []
+    for bug_index, bug in enumerate(assigned_bugs):
+        for closed_index, closed in enumerate(closed_bugs):
+            if bug['number'] == closed['number']:
+                #bug was closed at some point in time
+                if int(bug['internalDate']) > int(closed['internalDate']):
+                    #bug was reopened
+                    break
+                elif int(bug['internalDate']) < int(closed['internalDate']):
+                    #bug was fixed and closed
+                    bugs_to_remove.append(bug_index)
+                    break
+                else:
+                    #bug assigned and closed at the same time
+                    bugs_to_remove.append(bug_index)
+                    break
+            else:
+                #no match
+                pass
+    for index in reversed(bugs_to_remove):
+        assigned_bugs.pop(index)
     for bug in assigned_bugs:
-        print(bug)
-    print(len(assigned_bugs))
+        print(bug['number']," ",bug['assignment'])
+
 
 if __name__ == '__main__':
     main()
